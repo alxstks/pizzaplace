024e73e7ff6076b98b35668e0189a466cb9ec0c1
Your Name <you@example.com>
2022-01-30 23:24:10 -0600

    first commit

---
+++ pizza-place/src/main/java/com/alxstks/pizzaplace/PizzaPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.alxstks.pizzaplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaPlaceApplication.class, args);
+	}
+
+}
---
+++ pizza-place/src/test/java/com/alxstks/pizzaplace/PizzaPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alxstks.pizzaplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
